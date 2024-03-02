43a4df29ca8f1c5cb12ef87545559603f0620e52
CletsyMedia <Cletussam12@yahoo.com>
2024-03-02 21:07:48 +0100

    Select some Amenities to be comfortable!

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,14 +1,15 @@
+
 $(document).ready(function () {
-  let checkedAmenities = {};
+  let checkAmenities = {};
   $(document).on('change', "input[type='checkbox']", function () {
     if (this.checked) {
-      checkedAmenities[$(this).data('id')] = $(this).data('name');
+      checkAmenities[$(this).data('id')] = $(this).data('name');
     } else {
-      delete checkedAmenities[$(this).data('id')];
+      delete checkAmenities[$(this).data('id')];
     }
-    let lst = Object.values(checkedAmenities);
+    let lst = Object.values(checkAmenities);
     if (lst.length > 0) {
-      $('div.amenities > h4').text(Object.values(checkedAmenities).join(', '));
+      $('div.amenities > h4').text(Object.values(checkAmenities).join(', '));
     } else {
       $('div.amenities > h4').html('&nbsp;');
     }
